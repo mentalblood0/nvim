5dcbc2fcb736232f435f6414f57e56bef8c0cf0c
necheporenko_s_iu <necheporenko_s_iu@goznak.ru>
2023-12-28 16:55:31 +0300

    more binds for harpoon (up to 7 files from numbers keys)

--- lua/sameer/plugins/harpoon2.lua
+++ lua/sameer/plugins/harpoon2.lua
@@ -1,30 +1,39 @@
 return {
-  "ThePrimeagen/harpoon",
-  branch = "harpoon2",
-  requires = { { "nvim-lua/plenary.nvim" } },
-  config = function()
-    local harpoon = require("harpoon")
+	"ThePrimeagen/harpoon",
+	branch = "harpoon2",
+	requires = { { "nvim-lua/plenary.nvim" } },
+	config = function()
+		local harpoon = require("harpoon")
 
-    harpoon:setup()
+		harpoon:setup()
 
-    vim.keymap.set("n", "<leader>a", function()
-      harpoon:list():append()
-    end)
-    vim.keymap.set("n", "<C-e>", function()
-      harpoon.ui:toggle_quick_menu(harpoon:list())
-    end)
+		vim.keymap.set("n", "<leader>a", function()
+			harpoon:list():append()
+		end)
+		vim.keymap.set("n", "<C-e>", function()
+			harpoon.ui:toggle_quick_menu(harpoon:list())
+		end)
 
-    vim.keymap.set("n", "1", function()
-      harpoon:list():select(1)
-    end)
-    vim.keymap.set("n", "2", function()
-      harpoon:list():select(2)
-    end)
-    vim.keymap.set("n", "3", function()
-      harpoon:list():select(3)
-    end)
-    vim.keymap.set("n", "4", function()
-      harpoon:list():select(4)
-    end)
-  end,
+		vim.keymap.set("n", "1", function()
+			harpoon:list():select(1)
+		end)
+		vim.keymap.set("n", "2", function()
+			harpoon:list():select(2)
+		end)
+		vim.keymap.set("n", "3", function()
+			harpoon:list():select(3)
+		end)
+		vim.keymap.set("n", "4", function()
+			harpoon:list():select(4)
+		end)
+		vim.keymap.set("n", "5", function()
+			harpoon:list():select(5)
+		end)
+		vim.keymap.set("n", "6", function()
+			harpoon:list():select(6)
+		end)
+		vim.keymap.set("n", "7", function()
+			harpoon:list():select(7)
+		end)
+	end,
 }
