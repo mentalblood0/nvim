04e14a0a7a3f41a2b84cde08ba69dc83366c37d3
necheporenko_s_iu <necheporenko_s_iu@goznak.ru>
2023-12-29 13:37:41 +0300

    remove unused modules

--- lua/sameer/plugins/lsp/lspconfig.lua
+++ lua/sameer/plugins/lsp/lspconfig.lua
@@ -1,136 +1,129 @@
 return {
-  "neovim/nvim-lspconfig",
-  event = { "BufReadPre", "BufNewFile" },
-  dependencies = {
-    "hrsh7th/cmp-nvim-lsp",
-    { "antosha417/nvim-lsp-file-operations", config = true },
-  },
-  config = function()
-    local lspconfig = require("lspconfig")
-    local cmp_nvim_lsp = require("cmp_nvim_lsp")
+	"neovim/nvim-lspconfig",
+	event = { "BufReadPre", "BufNewFile" },
+	dependencies = {
+		"hrsh7th/cmp-nvim-lsp",
+		{ "antosha417/nvim-lsp-file-operations", config = true },
+	},
+	config = function()
+		local lspconfig = require("lspconfig")
+		local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
-    local keymap = vim.keymap
-    local opts = { noremap = true, silent = true }
+		local keymap = vim.keymap
+		local opts = { noremap = true, silent = true }
 
-    local on_attach = function(_, bufnr)
-      opts.buffer = bufnr
+		local on_attach = function(_, bufnr)
+			opts.buffer = bufnr
 
-      opts.desc = "Show LSP references"
-      keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts)
+			opts.desc = "Show LSP references"
+			keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts)
 
-      opts.desc = "Go to declaration"
-      keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+			opts.desc = "Go to declaration"
+			keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
 
-      opts.desc = "Show LSP definitions"
-      keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)
+			opts.desc = "Show LSP definitions"
+			keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)
 
-      opts.desc = "Show LSP implementations"
-      keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts)
+			opts.desc = "Show LSP implementations"
+			keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts)
 
-      opts.desc = "Show LSP type definitions"
-      keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts)
+			opts.desc = "Show LSP type definitions"
+			keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts)
 
-      opts.desc = "Smart rename"
-      keymap.set("n", "<F2>", vim.lsp.buf.rename, opts)
+			opts.desc = "Smart rename"
+			keymap.set("n", "<F2>", vim.lsp.buf.rename, opts)
 
-      opts.desc = "Show buffer diagnostics"
-      keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts)
+			opts.desc = "Show buffer diagnostics"
+			keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts)
 
-      opts.desc = "Restart LSP"
-      keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts)
+			opts.desc = "Restart LSP"
+			keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts)
 
-      local function quickfix()
-        vim.lsp.buf.code_action({
-          filter = function(a)
-            return a.isPreferred
-          end,
-          apply = true,
-        })
-      end
-      opts.desc = "Quickfix"
-      keymap.set("n", "<leader>qf", quickfix, opts)
+			local function quickfix()
+				vim.lsp.buf.code_action({
+					filter = function(a)
+						return a.isPreferred
+					end,
+					apply = true,
+				})
+			end
+			opts.desc = "Quickfix"
+			keymap.set("n", "<leader>qf", quickfix, opts)
 
-      opts.desc = "Switch header/source"
-      keymap.set("n", "<leader>gs", "<Cmd>ClangdSwitchSourceHeader<CR>", opts)
-    end
+			opts.desc = "Switch header/source"
+			keymap.set("n", "<leader>gs", "<Cmd>ClangdSwitchSourceHeader<CR>", opts)
+		end
 
-    local capabilities = cmp_nvim_lsp.default_capabilities()
+		local capabilities = cmp_nvim_lsp.default_capabilities()
 
-    local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-    for type, icon in pairs(signs) do
-      local hl = "DiagnosticSign" .. type
-      vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-    end
+		local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
+		for type, icon in pairs(signs) do
+			local hl = "DiagnosticSign" .. type
+			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+		end
 
-    lspconfig["html"].setup({
-      capabilities = capabilities,
-      on_attach = on_attach,
-      filetypes = { "html", "ejs" },
-    })
+		lspconfig["html"].setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+			filetypes = { "html", "ejs" },
+		})
 
-    lspconfig["tsserver"].setup({
-      capabilities = capabilities,
-      on_attach = on_attach,
-      init_options = {
-        preferences = {
-          disableSuggestions = true,
-        },
-      },
-    })
+		lspconfig["tsserver"].setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+			init_options = {
+				preferences = {
+					disableSuggestions = true,
+				},
+			},
+		})
 
-    lspconfig["cssls"].setup({
-      capabilities = capabilities,
-      on_attach = on_attach,
-    })
+		lspconfig["cssls"].setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+		})
 
-    lspconfig["emmet_ls"].setup({
-      capabilities = capabilities,
-      on_attach = on_attach,
-      filetypes = { "html", "typescriptreact", "javascriptreact", "css", "scss" },
-    })
+		lspconfig["emmet_ls"].setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+			filetypes = { "html", "typescriptreact", "javascriptreact", "css", "scss" },
+		})
 
-    lspconfig["pyright"].setup({
-      capabilities = capabilities,
-      on_attach = on_attach,
-      filetypes = { "python" },
-    })
+		lspconfig["pyright"].setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+			filetypes = { "python" },
+		})
 
-    lspconfig["solargraph"].setup({
-      capabilities = capabilities,
-      on_attach = on_attach,
-      root_dir = lspconfig.util.root_pattern("Gemfile", ".git", "."),
-      filetypes = { "ruby" },
-    })
+		lspconfig["graphql"].setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+			root_dir = lspconfig.util.root_pattern(".graphqlconfig", ".graphqlrc", "package.json"),
+			filetypes = { "graphql", "gql" },
+		})
 
-    lspconfig["graphql"].setup({
-      capabilities = capabilities,
-      on_attach = on_attach,
-      root_dir = lspconfig.util.root_pattern(".graphqlconfig", ".graphqlrc", "package.json"),
-      filetypes = { "graphql", "gql" },
-    })
+		lspconfig["lua_ls"].setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+			settings = {
+				Lua = {
+					diagnostics = {
+						globals = { "vim" },
+					},
+					workspace = {
+						library = {
+							[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+							[vim.fn.stdpath("config") .. "/lua"] = true,
+						},
+					},
+				},
+			},
+		})
 
-    lspconfig["lua_ls"].setup({
-      capabilities = capabilities,
-      on_attach = on_attach,
-      settings = {
-        Lua = {
-          diagnostics = {
-            globals = { "vim" },
-          },
-          workspace = {
-            library = {
-              [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-              [vim.fn.stdpath("config") .. "/lua"] = true,
-            },
-          },
-        },
-      },
-    })
-
-    lspconfig["clangd"].setup({
-      capabilities = capabilities,
-      on_attach = on_attach,
-      filetypes = { "c", "cpp", "objc", "objcpp" },
-    })
-  end,
+		lspconfig["clangd"].setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+			filetypes = { "c", "cpp", "objc", "objcpp" },
+		})
+	end,
 }
--- lua/sameer/plugins/lsp/mason.lua
+++ lua/sameer/plugins/lsp/mason.lua
@@ -1,52 +1,50 @@
 return {
-  "williamboman/mason.nvim",
-  dependencies = {
-    "williamboman/mason-lspconfig.nvim",
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-  },
-  config = function()
-    local mason = require("mason")
-    local mason_lspconfig = require("mason-lspconfig")
-    local mason_tool_installer = require("mason-tool-installer")
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
+	config = function()
+		local mason = require("mason")
+		local mason_lspconfig = require("mason-lspconfig")
+		local mason_tool_installer = require("mason-tool-installer")
 
-    mason.setup({
-      ui = {
-        icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗",
-        },
-      },
-    })
+		mason.setup({
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		})
 
-    mason_lspconfig.setup({
-      ensure_installed = {
-        "tsserver",
-        "html",
-        "cssls",
-        "lua_ls",
-        "emmet_ls",
-        "pyright",
-        "graphql",
-        "solargraph",
-        "clangd",
-      },
-      automatic_installation = true,
-    })
+		mason_lspconfig.setup({
+			ensure_installed = {
+				"tsserver",
+				"html",
+				"cssls",
+				"lua_ls",
+				"emmet_ls",
+				"pyright",
+				"graphql",
+				"clangd",
+			},
+			automatic_installation = true,
+		})
 
-    mason_tool_installer.setup({
-      ensure_installed = {
-        "prettierd",
-        "stylua",
-        "eslint_d",
-        "isort",
-        "black",
-        "eslint_d",
-        "rubocop",
-        "ruff",
-      },
+		mason_tool_installer.setup({
+			ensure_installed = {
+				"prettierd",
+				"stylua",
+				"eslint_d",
+				"isort",
+				"black",
+				"eslint_d",
+				"ruff",
+			},
 
-      automatic_installation = true,
-    })
-  end,
+			automatic_installation = true,
+		})
+	end,
 }
