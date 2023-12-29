87acaa1c8c28b08d737e98f16a5e1ff5e9f372c7
necheporenko_s_iu <necheporenko_s_iu@goznak.ru>
2023-12-29 13:38:24 +0300

    add neoclip

--- lua/sameer/plugins/telescope.lua
+++ lua/sameer/plugins/telescope.lua
@@ -1,53 +1,59 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-    "nvim-tree/nvim-web-devicons",
-  },
-  config = function()
-    local telescope = require("telescope")
-    local actions = require("telescope.actions")
+	"nvim-telescope/telescope.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+		local telescope = require("telescope")
+		local actions = require("telescope.actions")
 
-    telescope.setup({
-      defaults = {
-        layout_strategy = "vertical",
-        sorting_strategy = "ascending",
-        path_display = { "truncate " },
-        file_ignore_patterns = { "node_modules", "__pycache__", "build", "CMakeFiles" },
-        layout_config = {
-          width = { 0.75, max = 100 },
-        },
-        mappings = {
-          i = {
-            ["<C-k>"] = actions.move_selection_previous,
-            ["<C-j>"] = actions.move_selection_next,
-            ["<CR>"] = actions.select_default,
-          },
-        },
-      },
-    })
+		telescope.setup({
+			defaults = {
+				layout_strategy = "vertical",
+				sorting_strategy = "ascending",
+				path_display = { "truncate " },
+				file_ignore_patterns = { "node_modules", "__pycache__", "build", "CMakeFiles" },
+				layout_config = {
+					width = { 0.75, max = 100 },
+				},
+				mappings = {
+					i = {
+						["<C-k>"] = actions.move_selection_previous,
+						["<C-j>"] = actions.move_selection_next,
+						["<CR>"] = actions.select_default,
+					},
+				},
+			},
+		})
 
-    telescope.load_extension("fzf")
+		telescope.load_extension("fzf")
 
-    local keymap = vim.keymap
+		local keymap = vim.keymap
 
-    keymap.set("n", "<leader>f<S-TAB>", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
-    keymap.set("n", "<leader>f<TAB>", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
-    keymap.set(
-      "n",
-      "<leader>fa",
-      "<cmd>Telescope find_files hidden=true<cr>",
-      { desc = "Find files including hidden files" }
-    )
-    keymap.set("n", "<leader>fs", "<cmd>Telescope lsp_document_symbols<cr>", { desc = "Find symbols in the file" })
-    keymap.set(
-      "n",
-      "<leader>fd",
-      "<cmd>Telescope current_buffer_fuzzy_find<cr>",
-      { desc = "Find string in current buffer" }
-    )
+		keymap.set(
+			"n",
+			"<leader>f<S-TAB>",
+			"<cmd>Telescope grep_string<cr>",
+			{ desc = "Find string under cursor in cwd" }
+		)
+		keymap.set("n", "<leader>f<TAB>", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
+		keymap.set(
+			"n",
+			"<leader>fa",
+			"<cmd>Telescope find_files hidden=true<cr>",
+			{ desc = "Find files including hidden files" }
+		)
+		keymap.set("n", "<leader>fs", "<cmd>Telescope lsp_document_symbols<cr>", { desc = "Find symbols in the file" })
+		keymap.set(
+			"n",
+			"<leader>fd",
+			"<cmd>Telescope current_buffer_fuzzy_find<cr>",
+			{ desc = "Find string in current buffer" }
+		)
 
-    keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { desc = "Find buffers" })
-  end,
+		keymap.set("n", "<leader>fc", "<cmd>Telescope neoclip<cr>", { desc = "Find clip" })
+		keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { desc = "Find buffers" })
+	end,
 }
