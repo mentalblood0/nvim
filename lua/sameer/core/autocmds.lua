3b714ca0461f621baa109b058fb0558471d08253
necheporenko_s_iu <necheporenko_s_iu@goznak.ru>
2023-12-29 08:24:58 +0300

    disable folding completely

--- lua/sameer/core/autocmds.lua
+++ lua/sameer/core/autocmds.lua
@@ -2,3 +2,7 @@ vim.api.nvim_create_autocmd({ "BufLeave" }, { pattern = "*", command = "wa", nes
 -- vim.api.nvim_create_autocmd({ "CmdlineEnter" }, { pattern = "*", command = "set nu", nested = true })
 -- vim.api.nvim_create_autocmd({ "CmdlineLeave" }, { pattern = "*", command = "set nonu", nested = true })
 -- vim.api.nvim_create_autocmd({ "CursorMoved" }, { pattern = "*", command = "norm zz", nested = true })
+vim.api.nvim_create_autocmd(
+	{ "BufWritePost", "BufEnter" },
+	{ pattern = "*", command = "set nofoldenable foldmethod=manual foldlevelstart=99" }
+)
