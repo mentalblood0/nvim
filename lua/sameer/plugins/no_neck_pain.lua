105431c544e25e13a4dec2a4c073284663d6f379
necheporenko_s_iu <necheporenko_s_iu@goznak.ru>
2024-01-10 13:18:47 +0300

    set code area width 120 according to newly set clang-format option

--- lua/sameer/plugins/no_neck_pain.lua
+++ lua/sameer/plugins/no_neck_pain.lua
@@ -1,15 +1,15 @@
 return {
-  "shortcuts/no-neck-pain.nvim",
-  config = function()
-    local no_neck_pain = require("no-neck-pain")
-    no_neck_pain.setup({
-      width = 90,
-      autocmds = {
-        enableOnVimEnter = true,
-      },
-      mappings = {
-        enabled = true,
-      },
-    })
-  end,
+	"shortcuts/no-neck-pain.nvim",
+	config = function()
+		local no_neck_pain = require("no-neck-pain")
+		no_neck_pain.setup({
+			width = 120,
+			autocmds = {
+				enableOnVimEnter = true,
+			},
+			mappings = {
+				enabled = true,
+			},
+		})
+	end,
 }
