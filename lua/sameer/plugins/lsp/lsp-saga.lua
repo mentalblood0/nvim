7bbe8df91a485f0554140a9d1f3a361b122bb34c
mentalblood <neceporenkostepan@gmail.com>
2024-04-15 00:02:09 +0300

    remove trash

--- lua/sameer/plugins/lsp/lsp-saga.lua
+++ lua/sameer/plugins/lsp/lsp-saga.lua
@@ -1,7 +1,7 @@
 return {
 	"nvimdev/lspsaga.nvim",
 	dependencies = {
-		"nvim-treesitter/nvim-treesitter",
+		-- "nvim-treesitter/nvim-treesitter",
 		"nvim-tree/nvim-web-devicons",
 	},
 	config = function()
