955f95d4e884025f8ed5154e11c534a135caf256
Нечепоренко Степан <neceporenkostepan@gmail.com>
2024-07-24 15:20:33 +0300

    consistent line numbers toggling

--- lua/mentalblood/core/autocmds.lua
+++ lua/mentalblood/core/autocmds.lua
@@ -1,6 +1,11 @@
 vim.api.nvim_create_autocmd({ "BufLeave" }, { pattern = "*", command = "wa", nested = true })
--- vim.api.nvim_create_autocmd({ "CmdlineEnter" }, { pattern = "*", command = "set nu", nested = true })
--- vim.api.nvim_create_autocmd({ "CmdlineLeave" }, { pattern = "*", command = "set nonu", nested = true })
+vim.api.nvim_create_autocmd({ "CmdlineLeave" }, {
+	pattern = "*",
+	callback = function()
+		vim.opt.number = false
+	end,
+	nested = true,
+})
 -- vim.api.nvim_create_autocmd({ "CursorMoved" }, { pattern = "*", command = "norm zz", nested = true })
 vim.api.nvim_create_autocmd(
 	{ "BufWritePost", "BufEnter" },
