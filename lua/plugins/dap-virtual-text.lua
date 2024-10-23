0709518fee8699d9376a10ade908a05f56539ef3
mentalblood <neceporenkostepan@gmail.com>
2024-10-23 22:25:17 +0300

    remove trash

--- lua/plugins/dap-virtual-text.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"theHamsta/nvim-dap-virtual-text",
-	dependencies = { "nvim-treesitter/nvim-treesitter" },
-	config = function()
-		local vt = require("nvim-dap-virtual-text")
-		vt.setup()
-	end,
-}
