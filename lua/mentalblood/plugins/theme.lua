00ae625b28bfa5c8ff400dbfb098c2ec8e318277
mentalblood0 <necheporenko.stepan@yandex.ru>
2024-11-02 18:21:00 +0300

    some changes

--- lua/mentalblood/plugins/theme.lua
+++ lua/mentalblood/plugins/theme.lua
@@ -1,13 +1,21 @@
+-- return {
+-- 	"neanias/everforest-nvim",
+-- 	config = function()
+-- 		local theme = require("everforest")
+-- 		theme.setup({
+-- 			background = "hard",
+-- 			transparent_background_level = 0,
+-- 			italics = true,
+-- 			disable_italic_comments = false,
+-- 		})
+-- 		vim.cmd("colorscheme everforest")
+-- 	end,
+-- }
 return {
-	"neanias/everforest-nvim",
+	"cdmill/neomodern.nvim",
+	priority = 1000,
 	config = function()
-		local theme = require("everforest")
-		theme.setup({
-			background = "hard",
-			transparent_background_level = 0,
-			italics = true,
-			disable_italic_comments = false,
-		})
-		vim.cmd("colorscheme everforest")
+		require("neomodern").setup({ theme = "daylight" })
+		vim.cmd("colorscheme daylight")
 	end,
 }
