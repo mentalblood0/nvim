b70a5849fa1b81d62220fa1b43ef027f6d28afec
mentalblood0 <necheporenko.stepan@yandex.ru>
2024-11-23 16:21:19 +0300

    set roseprime as default colorscheme

--- lua/mentalblood/plugins/theme.lua
+++ lua/mentalblood/plugins/theme.lua
@@ -1,21 +1,8 @@
--- return {
--- 	"neanias/everforest-nvim",
--- 	config = function()
--- 		local theme = require("everforest")
--- 		theme.setup({
--- 			background = "hard",
--- 			transparent_background_level = 0,
--- 			italics = true,
--- 			disable_italic_comments = false,
--- 		})
--- 		vim.cmd("colorscheme everforest")
--- 	end,
--- }
 return {
 	"cdmill/neomodern.nvim",
 	priority = 1000,
 	config = function()
-		require("neomodern").setup({ theme = "daylight" })
-		vim.cmd("colorscheme daylight")
+		require("neomodern").setup({ theme = "roseprime" })
+		vim.cmd("colorscheme roseprime")
 	end,
 }
